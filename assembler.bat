ffb4d26f3129b1a34486070156a535128881b813
Emiliano Lancuba <lancuba.emiliano@gmail.com>
2020-07-13 00:48:40 -0300

    Ajustes pre entrega

--- assembler.bat
+++
@@ -1,8 +0,0 @@
-tasm assembler/numbers.asm
-tasm assembler/Final.asm
-tlink /3 assembler/Final.obj assembler/numbers.obj /v /s /m
-pause
-Final.exe
-pause
-del Final.obj
-del numbers.obj
--- funciones/funciones.c
+++ funciones/funciones.c
@@ -266,7 +266,7 @@ int generarFooter() {
 }
 
 int ensamblar() {
-    FILE * fp = fopen("./assembler/Final.asm", "w");
+    FILE * fp = fopen("Final.asm", "w");
     char buffer[100];
     if (fp == NULL)	
 		return 1;
